57906fc5c79c0dc62ce913bdc5f4254772d2b2f5
Nirmal <nirmal.panneerselvam@cognizant.com>
2021-12-23 05:20:57 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System; 
+    public class Program
+{
+    public static readonly string CTCPDelimiterString = "\x0001";
+    public static readonly char CTCPDelimiterChar = '\x0001';
+
+    public static void Main()
+    {
+        string text = "!sampletext";
+
+        Console.Write("  Using a char: ");
+        if (text.StartsWith(CTCPDelimiterChar) && text.EndsWith(CTCPDelimiterChar))
+        {
+            Console.WriteLine("got CTCP delimiters");
+        }
+        else
+        {
+            Console.WriteLine("did not get CTCP delimiters");
+        }
+
+        Console.Write("Using a string: ");
+        if (text.StartsWith(CTCPDelimiterString) && text.EndsWith(CTCPDelimiterString))
+        {
+            Console.WriteLine("got CTCP delimiters");
+        }
+        else
+        {
+            Console.WriteLine("did not get CTCP delimiters");
+        }
+    }
+}
